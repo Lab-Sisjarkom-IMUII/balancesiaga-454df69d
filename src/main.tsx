49d9341e63910b4dfa1fb800b6b3f654c94327ab
reza0038 <zakierpalevi038@gmail.com>
2026-01-05 13:01:48 +0700

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { registerSW } from "virtual:pwa-register";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<App />);
+
+registerSW({
+  immediate: true,
+});
